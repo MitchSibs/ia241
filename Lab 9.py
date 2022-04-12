3b3b44b1cf551f07dc85bbc57cccba990b4e3a5c
MitchSibs <98478585+MitchSibs@users.noreply.github.com>
2022-04-11 22:12:04 -0400

    Add files via upload

---
+++ Lab 9.py
@@ -0,0 +1,36 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: /Users/mitchsibs/Desktop/Python/lab.py ===============
+
+================ RESTART: /Users/mitchsibs/Desktop/Python/lab.py ===============
+3
+4
+
+================ RESTART: /Users/mitchsibs/Desktop/Python/lab.py ===============
+3
+4
+Traceback (most recent call last):
+  File "/Users/mitchsibs/Desktop/Python/lab.py", line 45, in <module>
+    print(my_cal.cal_f(5))
+TypeError: my_stat.cal_f() missing 1 required positional argument: 'n'
+
+================ RESTART: /Users/mitchsibs/Desktop/Python/lab.py ===============
+12
+60
+Traceback (most recent call last):
+  File "/Users/mitchsibs/Desktop/Python/lab.py", line 45, in <module>
+    print(my_cal.cal_f(5))
+TypeError: my_stat.cal_f() missing 1 required positional argument: 'n'
+
+================ RESTART: /Users/mitchsibs/Desktop/Python/lab.py ===============
+12
+60
+120
+20.0
+
+================ RESTART: /Users/mitchsibs/Desktop/Python/lab.py ===============
+12
+60
+120
+20.0
